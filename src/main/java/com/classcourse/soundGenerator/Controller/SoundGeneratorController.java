7fbd0aec5a9f05c6949ab6e0e4ae224485c78115
sshhsh <sshhsh@foxmail.com>
2018-06-04 21:36:36 +0800

    从音频数据中获取坐标

--- src/main/java/com/classcourse/soundGenerator/Controller/SoundGeneratorController.java
+++ src/main/java/com/classcourse/soundGenerator/Controller/SoundGeneratorController.java
@@ -5,16 +5,68 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.File;
 
 import com.classcourse.soundGenerator.WaveAccess.WaveFileReader;
+import com.classcourse.soundGenerator.WaveAnalyze.*;
 
 @RestController
 public class SoundGeneratorController {
+    static final int RATE_HZ = 48000;
+    static final int BUFFER_SIZE = RATE_HZ;
+
+    /**
+     * variables for calculation
+     */
+    private short[] pnData; //the PN sequence data
+    private short[] rawSound; //raw sound data
+    private double[] x;
+    private double[] y; //location of speakers
+    private double[] dd;
+    private LocationCalculator locationCalculator;
+    private CrossCorrelation correlation;
+    private CorrelationAnalyzer analyzer;
+
+    SoundGeneratorController() {
+        //init
+        rawSound = new short[BUFFER_SIZE];
+        locationCalculator = new LocationCalculator(3);
+        analyzer = new CorrelationAnalyzer(3);
+        correlation = new CrossCorrelation(BUFFER_SIZE);
+        x = new double[3];
+        y = new double[3];
+        dd = new double[2];
+
+        //location of 3 speakers
+        x[0] = 0;
+        x[1] = 0;
+        x[2] = 1;
+        y[0] = 1;
+        y[1] = 0;
+        y[2] = 0;
+        locationCalculator.updatePosition(x, y);
+
+        //get raw PN data
+        try {
+            InputStream in = this.getClass().getResourceAsStream("/data48000_18_20_200ms");
+            int length = in.available();
+            byte[] tmp = new byte[length];
+            //noinspection ResultOfMethodCallIgnored
+            in.read(tmp);
+            in.close();
+            pnData = new short[length / 2];
+            for (int i = 0; i < pnData.length; ++i) {
+                int tmp1 = tmp[2 * i] << 8;
+                int tmp2 = tmp[2 * i + 1] & 0x000000ff;
+                pnData[i] = (short) (tmp1 + tmp2);
+            }
+            System.out.println("Read raw data!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String generateSound () {
@@ -60,10 +112,28 @@ public class SoundGeneratorController {
     @RequestMapping(value = "/testAnalyze", method = RequestMethod.GET)
     public String testAnalyzeSound () {
         String result = "Success!";
-        WaveFileReader reader = new WaveFileReader("/Users/cplayer/Downloads/record.wav");
+        WaveFileReader reader = new WaveFileReader("/Users/cebiaozhu/Downloads/record.wav");
         int[][] data = reader.getData();
-        System.out.println(data.length);
-        System.out.println(data[0].length);
+
+        for (int i = 0; i < rawSound.length; ++i) {
+            if (i < data[0].length) {
+                rawSound[i] = (short) data[0][i];
+            } else {
+                rawSound[i] = 0;
+            }
+        }
+
+        double[] correlationData;
+        int[] resultIndex;
+        double[] locationResult;//location result
+        correlationData = correlation.getResult(rawSound, pnData);
+        resultIndex = analyzer.cal(correlationData);
+
+        dd[0] = -((double) (resultIndex[1] - resultIndex[0]) / RATE_HZ - 0.25) * 340;
+        dd[1] = -((double) (resultIndex[2] - resultIndex[1]) / RATE_HZ - 0.25) * 340;
+        locationResult = locationCalculator.cal(dd);
+        System.out.println(locationResult[0]);//x
+        System.out.println(locationResult[1]);//y
         return result;
     }
 
---
+++ src/main/java/com/classcourse/soundGenerator/WaveAnalyze/CorrelationAnalyzer.java
@@ -0,0 +1,137 @@
+package com.classcourse.soundGenerator.WaveAnalyze;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Created by sshhsh on 17-11-22.
+ * <p>
+ * CorrelationAnalyzer
+ * find signal positions in time line
+ */
+
+public class CorrelationAnalyzer {
+    static final private int window = 48000 / 1000 * 40; //window width for multi path analyze
+    static final private double rate = 10; //rate between largest correlation and the smallest accessible one
+    static final private int gap = 48000 / 1000 * 200; //minimum gap between two signals is 200ms
+
+    private int[] result;
+    private Queue<DataArea> dataAreas;
+
+    public class DataArea implements Comparable<DataArea> {
+        int startIndex;
+        int endIndex;
+        int maxValueIndex;
+        double maxValue;
+
+        DataArea(int startIndex, int endIndex, int maxValueIndex, double maxValue) {
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+            this.maxValueIndex = maxValueIndex;
+            this.maxValue = maxValue;
+        }
+
+        @Override
+        public int compareTo(DataArea dataArea) {
+            if (maxValue > dataArea.maxValue) {
+                return -1;
+            } else if (maxValue < dataArea.maxValue) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    /**
+     * init
+     *
+     * @param amount the amount of the pn sequences
+     */
+    public CorrelationAnalyzer(int amount) {
+        result = new int[amount];
+        for (int i = 0; i < amount; ++i) {
+            result[i] = -1;
+        }
+        dataAreas = new PriorityQueue<>();
+    }
+
+    /**
+     * find signal positions in time line
+     *
+     * @param correlationData correlation data
+     * @return time when signal occurs
+     */
+    public int[] cal(double[] correlationData) {
+        int count = 0;
+        int iMax = findMax(correlationData, 0, correlationData.length);
+        DataArea d = new DataArea(0, correlationData.length, iMax, correlationData[iMax]);
+        dataAreas.offer(d);
+        while (!dataAreas.isEmpty() && count < result.length) {
+            DataArea tmp = dataAreas.poll();
+            int iReal = findFirstPeak(correlationData, (tmp.maxValueIndex - window) > 0 ? (tmp.maxValueIndex - window) : 0, tmp.maxValueIndex, tmp.maxValue / rate);
+            result[count] = iReal;
+            count++;
+            if (tmp.maxValueIndex - gap > tmp.startIndex) {
+                int iMax1 = findMax(correlationData, tmp.startIndex, tmp.maxValueIndex - gap);
+                DataArea tmp1 = new DataArea(tmp.startIndex, tmp.maxValueIndex - gap, iMax1, correlationData[iMax1]);
+                dataAreas.offer(tmp1);
+            }
+            if (tmp.maxValueIndex + gap < tmp.endIndex) {
+                int iMax2 = findMax(correlationData, tmp.maxValueIndex + gap, tmp.endIndex);
+                DataArea tmp2 = new DataArea(tmp.maxValueIndex + gap, tmp.endIndex, iMax2, correlationData[iMax2]);
+                dataAreas.offer(tmp2);
+            }
+        }
+        while (!dataAreas.isEmpty()) {
+            dataAreas.poll();
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    /**
+     * find the index of maximum value among data
+     *
+     * @param data       data
+     * @param startIndex start index
+     * @param endIndex   end index
+     * @return the index of maximum value among data
+     */
+    private int findMax(double[] data, int startIndex, int endIndex) {
+        double tmp = -Double.MAX_VALUE;
+        int index = -1;
+        for (int i = startIndex; i < endIndex; ++i) {
+            if (data[i] > tmp) {
+                tmp = data[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * find the index of first peak among data
+     *
+     * @param data       data
+     * @param startIndex start index
+     * @param endIndex   end index
+     * @return the index of first peak among data
+     */
+    private int findFirstPeak(double[] data, int startIndex, int endIndex, double accessibleValue) {
+        int index = endIndex;
+        for (int i = startIndex; i < endIndex; ++i) {
+            if (data[i] > accessibleValue) {
+                for (int j = i + 1; j < endIndex; ++j) {
+                    if (data[j - 1] > data[j]) {
+                        index = j - 1;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        return index;
+    }
+}
